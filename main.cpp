6e7831741b79a718019d45f07336ee30eaceca89
yumia <madashi@foxmail.com>
2025-05-17 23:35:40 +0800

    复原

--- main.cpp
+++ main.cpp
@@ -3,13 +3,5 @@
 int main(){
 	int arr[] = {1, 2, 3, 4, 5};
 	int size = sizeof(arr)/sizeof(arr[0]);
-	return 0;
-<<<<<<< HEAD
-	// yumita改动
+	return 0; 
 }
-=======
-  // mcy改动
-  ?
-  // yumita ?改动 
-}
->>>>>>> 5c0e289e1e4241d11ca0579cceda5fdc31f5669b
